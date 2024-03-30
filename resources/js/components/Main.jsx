5b31f89ce73fa58218f27894b1666aa73fdf7e7a
Syofyan Zuhad <sofyanzuhad2@gmail.com>
2024-03-30 12:03:55 +0700

    Add new files and import dependencies for Laravel Reverb integration

---
+++ resources/js/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import '../../css/app.css';
+import ChatBox from "./ChatBox.jsx";
+
+if (document.getElementById('main')) {
+    const rootUrl = "http://laravel-11-reverb.test";
+    
+    ReactDOM.createRoot(document.getElementById('main')).render(
+        <React.StrictMode>
+            <ChatBox rootUrl={rootUrl} />
+        </React.StrictMode>
+    );
+}
